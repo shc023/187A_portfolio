ba94c414b28e0e85630741ab34bda54cf7331699
Firstname Lastname <scott6306@gmail.com>
2012-01-27 20:52:40 -0800

    Committing for the frst time

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,5 +6,27 @@ $(document).ready(function() {
     Simple image gallery. Use default settings
   */
   $('.fancybox').fancybox();
+  
+  $("#proj1_thumb").click( function(){
+		$("#gallery").fadeOut("fast",function(){
+			$("#proj1").fadeIn("fast");
+		});
+   });
+  $("#proj2_thumb").click( function(){
+		$("#gallery").fadeOut("fast",function(){
+			$("#proj2").fadeIn("fast");
+		});
+   });
+  $("#proj3_thumb").click( function(){
+		$("#gallery").fadeOut("fast",function(){
+			$("#proj3").fadeIn("fast");
+		});
+   });
+  
+  $(".back").click( function(){
+		$(".project_div").fadeOut("fast",function(){
+			$("#gallery").fadeIn("fast");
+		});						 
+	});
 
 });
